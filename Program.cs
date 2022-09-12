5ccc390bb7d68e96c1103364ec9b38dd2f727b36
Alexandru-Valentin Constantin <constantin.valentin0102@gmail.com>
2022-09-12 11:20:50 +0300

    Added the requirement and the main text in English

--- Program.cs
+++ Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace App1_Sir_Fibonacci
 {
     class Program
     {
-        /* Ex1. Să se determine și să se afișeze primii n termeni ai secvenței Fibonacci. (F0 = 0, F1 = 1, după care
+        /* Să se determine și să se afișeze primii n termeni ai secvenței Fibonacci. (F0 = 0, F1 = 1, după care
 se utilizează formula recursivă Fn = Fn-1 + Fn-2).*/
+
+        /* Determine and display the first n terms of the Fibonacci sequence. (F0 = 0, F1 = 1, after which
+the recursive formula Fn = Fn - 1 + Fn - 2 is used). */
 
         static void Main(string[] args)
         {
-            Console.Write("Sirul lui Fibonacci pentru n= ");
+            Console.Write("The Fibonacci sequence for n = ");
           
             int n = Convert.ToInt32(Console.ReadLine());
 
             // Initialize first elements
-            int F0 = 0, F1 = 1;
-            int F2;
+            int F0 = 0, F1 = 1, F2;
 
             // Fibonacci
             for (int i = 1; i<=n; i++)
@@ -23,11 +26,9 @@ se utilizează formula recursivă Fn = Fn-1 + Fn-2).*/
                 F2 = F1 + F0;
                 F0 = F1;
                 F1 = F2;
-
                 Console.Write(F0 + " ");
-
             }
-            
+
             Console.ReadKey();
         }
     }
