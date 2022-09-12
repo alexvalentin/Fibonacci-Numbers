0de09a44bb129a562869722de56bcd27a64c93a8
Alexandru-Valentin Constantin <constantin.valentin0102@gmail.com>
2022-09-12 11:30:33 +0300

    Added infinite while loop to calculate the Fibonacci sequence for many chosen values of n

--- Program.cs
+++ Program.cs
@@ -13,23 +13,27 @@ the recursive formula Fn = Fn - 1 + Fn - 2 is used). */
 
         static void Main(string[] args)
         {
-            Console.Write("The Fibonacci sequence for n = ");
-          
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            // Initialize first elements
-            int F0 = 0, F1 = 1, F2;
-
-            // Fibonacci
-            for (int i = 1; i<=n; i++)
+            while (true)
             {
-                F2 = F1 + F0;
-                F0 = F1;
-                F1 = F2;
-                Console.Write(F0 + " ");
-            }
+                Console.Write("The Fibonacci sequence for n = ");
 
-            Console.ReadKey();
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                // Initialize first elements
+                int F0 = 0, F1 = 1, F2;
+
+                // Fibonacci
+                for (int i = 1; i <= n; i++)
+                {
+                    F2 = F1 + F0;
+                    F0 = F1;
+                    F1 = F2;
+                    Console.Write(F0 + " ");
+                }
+
+                Console.Write("\n");
+                Console.Write("\n");
+            }  
         }
     }
 }
